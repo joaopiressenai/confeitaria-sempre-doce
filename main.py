f3d2de534b7de050ead080d0f8c20884a5855f15
joaopiressenai <140816716+joaopiressenai@users.noreply.github.com>
2024-07-26 10:10:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def inicial():
     return render_template("index.html")
 
 
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+
 if __name__ == "__main__":
     app.run()
